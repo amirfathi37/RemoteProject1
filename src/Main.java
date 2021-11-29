f2fd946383eb94c7200ba24b9fba047d5786fe65
amirfatgi <fathiamir37@gmail.com>
2021-11-29 11:05:37 +0330

    commit from exported project

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         //I add a new line
+        //I add a new line from exported project
     }
 }
